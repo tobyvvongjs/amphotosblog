c4f770b846aabff98f012d58265a546d70f6a590
tobyvvongjs <66780397+tobyvvongjs@users.noreply.github.com>
2020-07-06 12:48:41 +0300

    main page files
    main page with background changing script, simple header.

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+//PicturesArrayForBackground//
+var backgroundArray = [
+		'1.jpg',
+		'2.jpg',
+		'3.jpg',
+		'4.jpg'
+	];
+
+//BackgroundChangingScript//
+$(function () {
+        /* var i = 0;
+        $("#dvImage").css("background-image", "url(images/" + backgroundArray[i] + ")");
+        setInterval(function () {
+            i++;
+            if (i == backgroundArray.length) {
+                i = 0;
+            }
+            $("#dvImage").fadeOut("slow", function () {
+                $(this).css("background-image", "url(images/" + backgroundArray[i] + ")");
+                $(this).fadeIn("slow");
+            });
+        }, 3000); */
+});
